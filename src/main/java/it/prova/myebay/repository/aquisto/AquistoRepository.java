6d5cd58488e661e3a16646fedf3903caa69282d8
RobertoEsposito02 <robiespo02@libero.it>
2022-11-19 15:47:06 +0100

    aggiunti repository

---
+++ src/main/java/it/prova/myebay/repository/aquisto/AquistoRepository.java
@@ -0,0 +1,9 @@
+package it.prova.myebay.repository.aquisto;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.myebay.model.Aquisto;
+
+public interface AquistoRepository extends CrudRepository<Aquisto, Long>{
+
+}
